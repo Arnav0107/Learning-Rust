a646ef4ec4918c5ae014491514d75e466d1d5e25
Arnav0107 <arnavbansal.edu@gmail.com>
2026-08-09 06:57:22 +0000

    Add control flow: if/else, labeled loops, for loop over array

--- trial/src/main.rs
+++ trial/src/main.rs
@@ -131,25 +131,123 @@
 // }
 
 
-use std::io;
+// use std::io;
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("Please enter an array index.");
+
+//     let mut index = String::new();
+
+//     io::stdin()
+//         .read_line(&mut index)
+//         .expect("Failed to read line");
+
+//     let index: usize = index
+//         .trim()
+//         .parse()
+//         .expect("Index entered was not a number");
+
+//     let element = a[index];
+
+//     println!("The value of the element at index {index} is: {element}");
+// }
+
+
+// fn main() {
+//     println!("Hello, world!");
+
+//     another_function();
+// }
+
+// fn another_function() {
+//     println!("Another function.");
+// }
+
+// calling a plain function
+
+
+// fn main() {
+//     another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
+
+// fn main() {
+//     let x = plus_one(5);
+
+//     println!("The value of x is: {x}");
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1
+// }
+
+// // function with return variables
+
+
+
+// fn main() {
+//     let number = 3;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+
+
+// fn main() {
+//     let mut count = 0;
+//     'counting_up: loop {
+//         println!("count = {count}");
+//         let mut remaining = 10;
+
+//         loop {
+//             println!("remaining = {remaining}");
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let a = [10, 20, 30, 40, 50];
 
-    println!("Please enter an array index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
